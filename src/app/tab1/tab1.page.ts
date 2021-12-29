cf7e33f335bfbfc8f6cad4e8399652d089ec798d
Víctor Maree Redondo <victor.maree.redondo@copernic.alumnat.cat>
2021-12-29 13:32:14 +0100

    Basic functions Map with markers and route

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import "leaflet/dist/leaflet.css";
+import * as L from "leaflet";
+import "esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css";
+import "esri-leaflet-geocoder/dist/esri-leaflet-geocoder";
+import * as ELG from "esri-leaflet-geocoder";
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +12,110 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  map: L.Map;
+  propertyList = [];
+  apiKey = "AAPK0932e4c918794ef190d526dca145eafa4gkMAbVk_i2vyUy3k--v1M6oER9i6hoOtBDBFsk-xxs9CEyAGNrjFlHNKFMbVOEi";
+  basemapEnum = "ArcGIS:Navigation";//Streets
 
-  constructor() {}
+  constructor() { }
 
+  ionViewDidEnter() {
+    this.leafletMap();
+  }
+
+  leafletMap() {  
+
+    this.map = new L.Map('map1', {
+      minZoom: 2
+    }).setView([41.390205,2.154007], 14);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'edupala.com © Angular LeafLet',
+    }).addTo(this.map);
+
+    /*
+    L.esri.Vector.vectorBasemapLayer(this.basemapEnum, {
+      apiKey: this.apiKey
+    }).addTo(this.map);
+    */
+
+    // Create the select dropdown and add to the top-right of the map
+    L.Control.PlacesSelect = L.Control.extend({
+      onAdd: function(map) {
+        // Array of options
+        const optionsInfos = [["","Choose a category..."],["Coffee shop","Coffee shop"],["Gas station","Gas station"],["Food","Food"],["Hotel","Hotel"],["Parks and Outdoors","Parks and Outdoors"]];
+        // Create select
+        const select = L.DomUtil.create("select","");
+        select.setAttribute("id", "optionsSelect");
+        select.setAttribute("style", "font-size: 16px;padding:4px 8px;");
+        optionsInfos.forEach((optionsInfo) => {
+          let option = L.DomUtil.create("option");
+          option.value = optionsInfo[0];
+          option.innerHTML = optionsInfo[1];
+          select.appendChild(option);
+        });
+        return select;
+      },
+
+      onRemove: function(map) {
+        // Nothing to do here
+      }
+    });
+
+    L.control.placesSelect = function(opts) {
+      return new L.Control.PlacesSelect(opts);
+    }
+
+    L.control.placesSelect({
+      position: 'topright'
+    }).addTo(this.map);
+
+    const layerGroup = L.layerGroup().addTo(this.map);
+
+    var classicIcon = L.icon({
+      iconUrl: 'marker-icon.png',
+      //shadowUrl: 'leaf-shadow.png',
+  
+      iconSize:     [28, 45], // size of the icon
+      shadowSize:   [50, 64], // size of the shadow
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      shadowAnchor: [4, 62],  // the same for the shadow
+      popupAnchor:  [-8, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+    // When the selected where clause changes, do the geocode
+    const select = document.getElementById('optionsSelect');
+    select.addEventListener('change', () => {
+      var selectValue = (<HTMLInputElement>document.getElementById('optionsSelect')).value;
+
+      if(selectValue !== '') {
+        const geocoder = ELG.geocodeService({
+          apikey: this.apiKey
+        });
+        geocoder.geocode()
+          .category(selectValue)
+          .nearby(this.map.getCenter(), 10)
+          .run(function (error, response) {
+          if (error) {
+            return;
+          }
+
+          layerGroup.clearLayers();
+          response.results.forEach((searchResult) => {
+            L.marker(searchResult.latlng, {icon: classicIcon})
+              .addTo(layerGroup)
+              .bindPopup(`<b>${searchResult.properties.PlaceName}</b></br>${searchResult.properties.Place_addr}`);
+          });
+        });
+      }
+    });
+
+    
+
+    
+  }
+
+  ionViewWillLeave() {
+    this.map.remove();
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as Leaflet from 'leaflet';
+import L from 'leaflet';
+import { antPath } from 'leaflet-ant-path';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  map: Leaflet.Map;
+
 
   constructor() {}
 
-}
+  ionViewDidEnter() { this.leafletMap(); }
+
+  leafletMap() {
+    this.map = Leaflet.map('map').setView([28.644800, 77.216721], 5);
+    Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'edupala.com © Angular LeafLet',
+    }).addTo(this.map);
+
+    var classicIcon = L.icon({
+      iconUrl: 'marker-icon.png',
+      //shadowUrl: 'leaf-shadow.png',
+  
+      iconSize:     [28, 45], // size of the icon
+      shadowSize:   [50, 64], // size of the shadow
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      shadowAnchor: [4, 62],  // the same for the shadow
+      popupAnchor:  [-8, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+    Leaflet.marker([28.6, 77], {icon: classicIcon}).addTo(this.map).bindPopup('Delhi').openPopup();
+    Leaflet.marker([34, 77], {icon: classicIcon}).addTo(this.map).bindPopup('Leh').openPopup();
+
+    antPath([[28.6, 77.216721], [34, 77.5771]],
+      { color: '#FF0000', weight: 5, opacity: 0.6 })
+      .addTo(this.map);
+  }
+
+  /** Remove map when we have multiple map object */
+  ngOnDestroy() {
+    this.map.remove();
+  }
+  
+  }
